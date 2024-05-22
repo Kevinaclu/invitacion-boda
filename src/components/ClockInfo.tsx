015d8d2cec76f85a45b7643dc1fff9d0e9551ef6
Kevin Acosta <kevin.acosta@spinbyoxxo.com.mx>
2024-05-22 01:30:34 -0700

    change styles

--- src/components/ClockInfo.tsx
+++ src/components/ClockInfo.tsx
@@ -5,14 +5,12 @@ type ClockInfoProps = {
 
 export default function ClockInfo({ difference, text }: ClockInfoProps) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col border-double rounded-md border-[4px] border-[#A47E3B] font-cormorant font-light">
       <div
         id="days"
-        className="w-20 h-16 bg-slate-400 text-2xl items-center justify-center flex text-white"
+        className="w-20 h-16 bg-white text-2xl items-center justify-center flex  flex-col"
       >
         {difference > 0 ? difference : 0}
-      </div>
-      <div className="flex flex-row space-x-4 mt-2 bg-slate-400 text-white">
         <p className="w-20 text-center text-sm">{text}</p>
       </div>
     </div>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        gloock: ["Gloock", "serif"],
+        loversQuarrel: ["Lovers Quarrel", "cursive"],
+        cormorant: ["Cormorant Garamond", "serif"],
+        baskerville: ["Libre Baskerville", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
