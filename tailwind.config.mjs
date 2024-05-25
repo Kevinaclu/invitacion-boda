23e0915d61b6333648ed3e73e1ab7f26f524ce62
kevinaclu <kevinaclu@gmail.com>
2024-05-25 11:15:40 -0700

    add photos

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 import defaultTheme from "tailwindcss/defaultTheme";
+import animations from "@midudev/tailwind-animations";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -18,5 +19,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [animations],
 };
