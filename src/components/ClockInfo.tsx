f2449a28095471da74df357370f36163b3eb7bdf
Kevin Acosta <kevin.acosta@spinbyoxxo.com.mx>
2024-05-25 09:26:31 -0700

    fix and add carrousel

--- src/components/ClockInfo.tsx
+++ src/components/ClockInfo.tsx
@@ -8,7 +8,7 @@ export default function ClockInfo({ difference, text }: ClockInfoProps) {
     <div className="flex flex-col border-double rounded-md border-[4px] border-[#A47E3B] font-cormorant font-light">
       <div
         id="days"
-        className="w-20 h-16 bg-white text-2xl items-center justify-center flex  flex-col"
+        className="w-16 h-16 bg-white text-2xl items-center justify-center flex  flex-col"
       >
         {difference > 0 ? difference : 0}
         <p className="w-20 text-center text-sm">{text}</p>
